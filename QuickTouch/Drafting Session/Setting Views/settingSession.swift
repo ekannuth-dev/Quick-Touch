8ca11e5565ed3dc98b4fb60837a6946b4f5b6d6d
ekannuth-dev <ekannuth@uwaterloo.ca>
2025-02-25 13:13:51 -0500

    readded data view and implemented logic for turning off interval color count below two

--- QuickTouch/Drafting Session/Setting Views/settingSession.swift
+++ QuickTouch/Drafting Session/Setting Views/settingSession.swift
@@ -11,21 +11,28 @@ import Foundation
 struct SettingSession: View {
     @ObservedObject var draftModel: sessionViewModel
     @State private var activeAlert: SessionAlerts?
+    
     var body: some View {
         VStack {
             TimerView(draftSession: draftModel)
             intervalView(draftModel: draftModel)
-            Toggle("Enable Interval Session", isOn: Binding (
+            Toggle("Enable Interval Session", isOn: Binding(
                 get: { draftModel.isIntervalSession },
                 set: { newValue in
-                    if newValue && draftModel.initialMin == 0 && draftModel.initialSec == 0 {
-                        activeAlert = .zeroTimeAlert
-                    }
-                    else if draftModel.intervalColor.count < 2 {
-                        activeAlert = .colorAlert
-                    }
-                    else {
-                        draftModel.isIntervalSession = newValue
+                    if newValue {
+                        // Checks when trying to enable interval session
+                        if draftModel.initialMin == 0 && draftModel.initialSec == 0 {
+                            activeAlert = .zeroTimeAlert
+                        }
+                        else if draftModel.intervalColor.count < 2 {
+                            activeAlert = .colorAlert
+                        }
+                        else {
+                            draftModel.isIntervalSession = newValue
+                        }
+                    } else {
+                        // When turning off interval session
+                        draftModel.isIntervalSession = false
                     }
                 }
             ))
@@ -36,6 +43,13 @@ struct SettingSession: View {
                 message: Text(alert.message),
                 dismissButton: .default(Text("OK"))
             )
+        }
+        // Add observer for intervalColor count changes
+        .onChange(of: draftModel.intervalColor.count) {oldCount, count in
+            if count < 2 && draftModel.isIntervalSession {
+                draftModel.isIntervalSession = false
+            //    activeAlert = .colorAlert
+            }
         }
     }
 }
